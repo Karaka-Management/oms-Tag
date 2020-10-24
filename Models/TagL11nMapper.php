a7efdc143b8023f379d4bd6173dcce70fd44ef3d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-10-24 17:52:24 +0200

    cs and phpstan fixes

--- Models/TagL11nMapper.php
+++ Models/TagL11nMapper.php
@@ -45,7 +45,7 @@ final class TagL11nMapper extends DataMapperAbstract
     /**
      * Has one relation.
      *
-     * @var array<string, array{mapper:string, self:string, by?:string, column?:string}>
+     * @var array<string, array{mapper:string, external:string, by?:string, column?:string, conditional?:bool}>
      * @since 1.0.0
      */
     protected static array $ownsOne = [
