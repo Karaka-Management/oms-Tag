076e2c0026456ff6adef7f36c5981a6ad82f8e87
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-12-06 11:13:19 +0100

    tpl fixes, dont use printHtml for none-string types

--- Theme/Backend/Components/TagSelector/base.tpl.php
+++ Theme/Backend/Components/TagSelector/base.tpl.php
@@ -1,10 +1,10 @@
 <div class="ipt-wrap">
     <div class="ipt-first">
         <span class="input">
-            <button type="button" id="<?= $this->printHtml($this->getId()); ?>-book-button" data-action='[
+            <button type="button" id="<?= $this->getId(); ?>-book-button" data-action='[
                 {
                     "key": 1, "listener": "click", "action": [
-                        {"key": 1, "type": "dom.popup", "selector": "#acc-grp-tpl", "aniIn": "fadeIn", "id": "<?= $this->printHtml($this->getId()); ?>"},
+                        {"key": 1, "type": "dom.popup", "selector": "#acc-grp-tpl", "aniIn": "fadeIn", "id": "<?= $this->getId(); ?>"},
                         {"key": 2, "type": "message.request", "uri": "<?= \phpOMS\Uri\UriFactory::build('{/prefix}tag?filter=some&limit=10'); ?>", "method": "GET", "request_type": "json"},
                         {"key": 3, "type": "dom.table.append", "id": "acc-table", "aniIn": "fadeIn", "data": [], "bindings": {"id": "id", "name": "title"}, "position": -1},
                         {"key": 4, "type": "message.request", "uri": "<?= \phpOMS\Uri\UriFactory::build('{/prefix}tag?filter=some&limit=10'); ?>", "method": "GET", "request_type": "json"},
@@ -12,19 +12,19 @@
                     ]
                 }
             ]' formaction=""><i class="fa fa-book"></i></button>
-            <div class="advancedInput wf-100" id="<?= $this->printHtml($this->getId()); ?>">
-                <input autocomplete="off" class="input" type="text" id="i<?= $this->printHtml($this->getId()); ?>" placeholder="&#xf007; Guest"
+            <div class="advancedInput wf-100" id="<?= $this->getId(); ?>">
+                <input autocomplete="off" class="input" type="text" id="i<?= $this->getId(); ?>" placeholder="&#xf007; Guest"
                     data-emptyAfter="true"
                     data-autocomplete="off"
-                    data-src="api/tag/find?search={!#i<?= $this->printHtml($this->getId()); ?>}">
-                <div id="<?= $this->printHtml($this->getId()); ?>-dropdown" class="dropdown" data-active="true">
+                    data-src="api/tag/find?search={!#i<?= $this->getId(); ?>}">
+                <div id="<?= $this->getId(); ?>-dropdown" class="dropdown" data-active="true">
                     <table class="default">
                         <thead>
                             <tr>
                                 <td>ID<i class="sort-asc fa fa-chevron-up"></i><i class="sort-desc fa fa-chevron-down"></i>
                                 <td>Title<i class="sort-asc fa fa-chevron-up"></i><i class="sort-desc fa fa-chevron-down"></i>
                         <tbody>
-                            <template id="<?= $this->printHtml($this->getId()); ?>-rowElement" class="rowTemplate">
+                            <template id="<?= $this->getId(); ?>-rowElement" class="rowTemplate">
                                 <tr tabindex="-1">
                                     <td data-tpl-text="/id" data-tpl-value="/id" data-value=""></td>
                                     <td data-tpl-text="/title" data-tpl-value="/title" data-value=""></td>
@@ -38,8 +38,8 @@
     </div>
     <div class="ipt-second"><button><?= $this->getHtml('Select', '0', '0'); ?></button></div>
 </div>
-<div class="box" data-form="<?= $this->printHtml($this->getForm()); ?>" id="<?= $this->printHtml($this->getId()); ?>-tags" data-limit="0" data-active="true">
-    <template id="<?= $this->printHtml($this->getId()); ?>-tagTemplate">
+<div class="box" data-form="<?= $this->printHtml($this->getForm()); ?>" id="<?= $this->getId(); ?>-tags" data-limit="0" data-active="true">
+    <template id="<?= $this->getId(); ?>-tagTemplate">
         <span class="tag" data-tpl-value="/id" data-value="" data-uuid="" data-name="<?= $this->printHtml($this->name); ?>" style="background: {/color};">
             <i class="fa fa-times"></i>
             <span data-tpl-text="/id" data-name="id" data-tpl-value="/id" data-value=""></span>
