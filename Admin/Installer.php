c269db96c0b8b9b164b17d0bdd54a83cc076ce09
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-10 16:07:06 +0200

    phpstan fixes

--- Admin/Installer.php
+++ Admin/Installer.php
@@ -46,7 +46,7 @@ final class Installer extends InstallerAbstract
     {
         parent::install($dbPool, $info, $cfgHandler);
 
-        self::installDefaultColors($info, $cfgHandler);
+        self::installDefaultColors();
     }
 
     /**
@@ -57,6 +57,6 @@ final class Installer extends InstallerAbstract
     private static function installDefaultColors() : void
     {
         $setting = new Setting();
-        SettingMapper::create($setting->with(0, '1007500001', '#ff000000;#ff00ff00;#ffffffff', '(#[a-fA-F0-9]{8};)*(#[a-fA-F0-9]{8})', 'Tag'));
+        SettingMapper::create($setting->with(0, '1007500001', '#ff000000;#ff00ff00;#ffffffff', '(#[a-fA-F0-9]{8};)*(#[a-fA-F0-9]{8})', null, 'Tag'));
     }
 }
