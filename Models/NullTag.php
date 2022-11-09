dc07454050534596f50e68d8bed16b5babf28f9d
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-11-09 23:02:52 +0100

    reduce serialization of null models

--- Models/NullTag.php
+++ Models/NullTag.php
@@ -35,4 +35,12 @@ final class NullTag extends Tag
     {
         $this->id = $id;
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize() : mixed
+    {
+        return ['id' => $this->id];
+    }
 }
--- Models/NullTagL11n.php
+++ Models/NullTagL11n.php
@@ -36,4 +36,12 @@ final class NullTagL11n extends TagL11n
         $this->id = $id;
         parent::__construct();
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize() : mixed
+    {
+        return ['id' => $this->id];
+    }
 }
