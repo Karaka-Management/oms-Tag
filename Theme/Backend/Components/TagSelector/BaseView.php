d3e6484f5eb0a805e7a45ce1edac1de028da45e9
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2024-01-30 21:29:21 +0000

    bump

--- Theme/Backend/Components/TagSelector/BaseView.php
+++ Theme/Backend/Components/TagSelector/BaseView.php
@@ -65,7 +65,7 @@ class BaseView extends View
     /**
      * {@inheritdoc}
      */
-    public function __construct(?L11nManager $l11n = null, RequestAbstract $request, ResponseAbstract $response)
+    public function __construct(L11nManager $l11n, RequestAbstract $request, ResponseAbstract $response)
     {
         parent::__construct($l11n, $request, $response);
         $this->setTemplate('/Modules/Tag/Theme/Backend/Components/TagSelector/base');
